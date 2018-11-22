15bdfd4dc65e43300a21a1765535d7b151b40391
brend <7brend7@gmail.com>
2018-11-23 00:05:39 +0200

    change icons; implement startForm;

--- src/js/components/StartForm.vue
+++ src/js/components/StartForm.vue
@@ -1,8 +1,31 @@
 <template>
-    <main id="app" class="startForm">
-        <input type="file" @change="processFile($event)" />
-        <input v-model="password" type="password" />
-        <button type="button" @click="openDbFile">Go</button>
+    <main id="app">
+        <div class="startForm">
+            <div class="startForm--logo">
+                <img src="/static/img/Logoedit.png" alt="logo">
+            </div>
+            <div class="startForm--divider"></div>
+            <div class="startForm--form">
+                <div class="startForm--formAlign">
+                    <label>Please choose your existing kdbx database:</label>
+
+                    <label class="startForm--formFileContainer">
+                        <input type="text" placeholder="Click here to trigger the file uploader!" />
+                        <input type="file" @change="processFile($event)" />
+                    </label>
+
+                    <label>
+                        Don't have such? <a href="#" @click="generateDb">click here</a>.
+                    </label>
+
+                    <div class="startForm--divider-H"></div>
+
+                    <label>Password for database:</label>
+                    <input v-model="password" type="password" />
+                    <button type="button" @click="openDbFile">Continue</button>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -17,6 +40,11 @@
             }
         },
         methods: {
+            generateDb: function(e) {
+
+
+                e.preventDefault();
+            },
             openDbFile: function() {
                 const reader = new FileReader();
                 const passwd = this.password;
