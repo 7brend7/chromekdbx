8cfa0ae6a9b43d3121f6d662afb8b9f14e93720c
Borys Anikiyenko <7brend7@gmail.com>
2018-11-23 18:31:02 +0200

    improve styles

--- src/js/components/StartForm.vue
+++ src/js/components/StartForm.vue
@@ -10,19 +10,19 @@
                     <label>Please choose your existing kdbx database:</label>
 
                     <label class="startForm--formFileContainer">
-                        <input type="text" placeholder="Click here to trigger the file uploader!" />
+                        <input type="text" placeholder="Click here to trigger the file uploader" v-model="fileName" :class="[{'error': checkError('db')}]" />
                         <input type="file" @change="processFile($event)" />
                     </label>
 
-                    <label>
+                    <label class="startForm--formFileContainerNote">
                         Don't have such? <a href="#" @click="generateDb">click here</a>.
                     </label>
 
                     <div class="startForm--divider-H"></div>
 
                     <label>Password for database:</label>
-                    <input v-model="password" type="password" />
-                    <button type="button" @click="openDbFile">Continue</button>
+                    <input v-model="password" type="password"  :class="[{'error': checkError('password')}]" />
+                    <button class="startForm--formFileContainerContinueBtn" type="button" @click="openDbFile">Continue</button>
                 </div>
             </div>
         </div>
@@ -35,17 +35,37 @@
     export default {
         data() {
             return {
+                fileName: '',
                 password: null,
                 db: null,
+                errors: {}
             }
         },
         methods: {
+            checkError: function(name) {
+                return typeof this.errors[name] != 'undefined';
+            },
             generateDb: function(e) {
 
 
                 e.preventDefault();
             },
+            validate: function() {
+                this.errors = {};
+
+                for (let value of ['db', 'password']) {
+                    if(!this[value]) {
+                        this.errors[value] = true;
+                    }
+                }
+
+                return Object.keys(this.errors).length == 0;
+            },
             openDbFile: function() {
+                if (!this.validate()) {
+                    return;
+                }
+
                 const reader = new FileReader();
                 const passwd = this.password;
 
@@ -62,6 +82,7 @@
             },
             processFile: function() {
                 this.db = event.target.files[0];
+                this.fileName = this.db.name;
             }
         }
     }
